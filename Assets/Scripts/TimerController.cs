75345fb2689526aa022ea75129d6b7271fc58581
Lilum888 <jparrott@mail.bradley.edu>
2025-02-02 12:47:51 -0600

    Timer
    Timer working in my test scene,

---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TimerController : MonoBehaviour
+{
+    bool stopwatchActive = false;
+    float currentTime=0;
+    [SerializeField] TMP_Text timetext;
+
+    private void Start()
+    {
+        StartStopwatch();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(stopwatchActive==true)
+        {
+            currentTime = currentTime + Time.deltaTime;
+        }
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        timetext.text = time.ToString(@"mm\:ss\:fff");
+    }
+    public void StartStopwatch()
+    {
+        stopwatchActive = true;
+    }
+    public void StopStopwatch()
+    {
+        stopwatchActive = false;
+    }
+}
