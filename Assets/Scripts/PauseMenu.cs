62effa4efe74381e248e332adf30573aa7145390
The25th <jack.lloyd.fried@gmail.com>
2025-02-03 20:17:57 -0600

    Pause screen implemented

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -6,21 +6,83 @@
 // Brief Description : Controls the pause screen during gameplay.
 *****************************************************************************/
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Setting variables
+    [SerializeField] private PlayerInput PlayerInputInstance;
+    private bool isPause = false;
+
+    [SerializeField] private Image PauseBG;
+    [SerializeField] private GameObject ButtonGroup;
+
+
+    /// <summary>
+    /// Automatically disable the pause menu on start
+    /// </summary>
     void Start()
     {
-        
+        PauseBG.enabled = false;
+        ButtonGroup.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    /// <summary>
+    /// Reads the "Pause" input and stops the game's processing until unpaused
+    /// </summary>
+    void OnPause()
     {
-        
+        PauseActivate();
+    }
+
+    /// <summary>
+    /// Resume the game when the pause menu "Resume" button is pressed
+    /// </summary>
+    public void Resume()
+    {
+        PauseActivate();
+    }
+
+    /// <summary>
+    /// Reloads the current level when the pause menu "Restart" button is pressed
+    /// </summary>
+    public void Restart()
+    {
+        PauseActivate(); //Unpauses the game for the reload
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); //Gets the name of the currently active scene
+    }
+
+    /// <summary>
+    /// Return to the main menu when the pause menu "Main Menu" button is pressed
+    /// </summary>
+    public void MainMenu()
+    {
+        PauseActivate(); //Unpauses the game before returning
+        SceneManager.LoadScene("Menu");
+    }
+
+    /// <summary>
+    /// Pause function to be easily reused in multiple places
+    /// </summary>
+    private void PauseActivate()
+    {
+        if (isPause == false)
+        {
+            Time.timeScale = 0;
+            PauseBG.enabled = true;
+            ButtonGroup.SetActive(true);
+            isPause = true;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            PauseBG.enabled = false;
+            ButtonGroup.SetActive(false);
+            isPause = false;
+        }
     }
 }
