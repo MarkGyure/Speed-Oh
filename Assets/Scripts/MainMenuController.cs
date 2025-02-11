d7c156a2312db733836686d57fae4325000a67f5
HiddenHunter462 <sammycross5@icloud.com>
2025-02-10 23:43:09 -0600

    Got animations working

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
+using System;
 
 public class MainMenuController : MonoBehaviour
 {
@@ -10,15 +11,32 @@ public class MainMenuController : MonoBehaviour
 
     internal UIShowHide ShowingMainMenuUI;
 
+    private PlayerInput _playerInput;
+
+    //Start is called before the first frame update
     void Start()
     {
         Instance = this;
     }
 
-    private void CloseCurrentMenu()
+    private InputAction back;
+
+    //Start is called before the first frame update
+    /*void Awake()
+    {
+        back = _playerInput.currentActionMap.FindAction("Back");
+        back.started += CloseCurrentMenu;
+    }*/
+
+    private void CloseCurrentMenu(InputAction.CallbackContext context)
     {
         if (ShowingMainMenuUI == null) return;
 
         ShowingMainMenuUI.ButtonPress();
     }
+
+    /*private void OnDestroy()
+    {
+        back.started -= Close
+    }*/
 }
